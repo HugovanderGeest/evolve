7d577596fff1742eaa3f4f571aa2e06afe7d55ec
Hugo van der Geest <Hugo.vdg@outlook.com>
2020-09-18 17:48:01 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// js
+
+// dit zijn wij
+
+// -------------------
+
+function verdwijn(){
+  var x = document.getElementById("muisklik");
+  if (x.style.right === "0"){
+    x.style.right = "-100vw";
+  } else {
+    x.style.right = "0";
+  }
+}
